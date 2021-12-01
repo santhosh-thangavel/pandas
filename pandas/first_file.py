32f671cc2e9184e3d35d07ba47b2bd364075fb1c
santhosh thangavel <santhoshk.thangavel@gmail.com>
2021-12-01 04:44:58 +0000

    first commit

---
+++ pandas/first_file.py
@@ -0,0 +1,14 @@
+# names_i_want = ["London", "Paris"]
+# total_names = ["London", "Dubai", "Singapore"]
+
+# for names in total_names:
+#     if "London" in names or "Singapore" in names:
+#         continue
+#     print(names)
+
+# import pandas as pd
+import numpy as np
+
+my_numpy_array = np.random.randint(3)
+print(my_numpy_array)
+
